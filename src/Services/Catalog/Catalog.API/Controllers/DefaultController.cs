8e38a51cc3c55ac4992d4a35b94dac1ff223d8e0
Gerson Azabache <gerson_dunk@hotmail.com>
2022-10-31 17:42:16 -0500

    folder structure updated

---
+++ src/Services/Catalog/Catalog.API/Controllers/DefaultController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Catalog.API.Controllers
+{
+    [ApiController]
+    [Route("/")]
+    public class DefaultController
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "Running...";
+        }
+    }
+}
