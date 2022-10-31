a9891d4bc038b6aab781bcdf4e47dfb9a44cd5e8
Gerson Azabache <gerson_dunk@hotmail.com>
2022-10-31 18:11:27 -0500

    context

---
+++ src/Services/Catalog/Catalog.Database/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Catalog.Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catalog.Database
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        //Mis entidades
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductStock> Stocks { get; set; }
+    }
+}
